d23d02fba699452aaf8babfa003be55b07a1302c
Aryan raj <75358720+aryanraj2713@users.noreply.github.com>
2021-05-02 19:54:55 +0530

    Add files via upload

---
+++ digital clock.py
@@ -0,0 +1,19 @@
+import time
+from tkinter import *
+
+root = Tk()
+root.title("Digital Clock : ---Aryan Raj")
+root.resizable()
+lbl = Label(root)
+lbl.grid(row=0, column=0)
+
+
+def display():
+    time_get = time.strftime("%Y/%m/%d\n%I:%M:%S %p")
+    lbl.config(text=time_get, bg='black', fg='blue',
+               font=('Butterbelly', 100, 'bold'))
+    lbl.after(200, display)
+
+
+display()
+root.mainloop()
