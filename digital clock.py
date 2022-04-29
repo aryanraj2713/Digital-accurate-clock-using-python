2ee5ee633b1b3100ded47969f21cefb1303bfdb9
Aryan raj <aryanraj2713@gmail.com>
2022-04-29 17:08:37 +0530

    color change

--- digital clock.py
+++ digital clock.py
@@ -11,7 +11,7 @@ lbl.grid(row=0, column=0)
 
 def display():
     time_get = time.strftime("%Y/%m/%d\n%I:%M:%S %p")
-    lbl.config(text=time_get, bg='black', fg='blue',
+    lbl.config(text=time_get, bg='black', fg='red',
                font=('Butterbelly', 100, 'bold'))
     lbl.after(200, display)
 
